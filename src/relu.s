cf6a53086e9d15eee2a597e73895f8b62d4b5fc5
KaiIvring <3011973335@qq.com>
2025-08-18 15:40:38 +0800

    finish relu.s

--- src/relu.s
+++ src/relu.s
@@ -13,26 +13,35 @@
 #     this function terminates the program with error code 36
 # ==============================================================================
 relu:
+    ebreak
     # Prologue
-
+    addi t0, a1, -1
+    mv t1, x0
+    blt t0, x0, exit_code36
 
 loop_start:
+    blt t0, x0, loop_end
 
+    slli t2, t1, 2
+    add t2, t2, a0
+    lw t3, 0(t2)
 
-
-
-
-
+    addi t1, t1, 1
+    addi t0, t0, -1
+    bge t3, x0, loop_start
 
 
 loop_continue:
+    sw x0 0(t2)
+    j loop_start
 
 
 
 loop_end:
-
-
     # Epilogue
 
-
     jr ra
+
+exit_code36:
+    li a0, 36
+    j exit
