bb80033db034a8bc611ffbddb2a9071276c7ff7d
KaiIvring <3011973335@qq.com>
2025-08-21 10:27:21 +0800

    finished dot.s

--- src/dot.s
+++ src/dot.s
@@ -20,25 +20,46 @@
 dot:
 
     # Prologue
+    li t0, 1
+    blt a2, t0, exit_code36
+    blt a3, t0, exit_code37
+    blt a4, t0, exit_code37
+
+    li t1, 0        # array0 index
+    li t2, 0        # array1 index
+    li t6, 0        # result
 
 
 loop_start:
+    blt a2, t0, loop_end
 
+    slli t3, t1, 2
+    add t3, a0, t3
+    lw t4, 0(t3)    # t4 = arr0[t1]
 
+    slli t3, t2, 2
+    add t3, a1, t3
+    lw t5, 0(t3)    # t5 = arr0[t2]
 
+    mul t5, t4, t5
+    add t6, t6, t5
 
+    add t1, t1, a3
+    add t2, t2, a4
+    addi a2, a2, -1
 
-
-
-
-
-
-
+    j loop_start
 
 loop_end:
-
-
     # Epilogue
-
-
+    mv a0, t6
     jr ra
+
+
+exit_code36:
+    li a0, 36
+    j exit
+
+exit_code37:
+    li a0, 37
+    j exit
